be350e6bf12152769cc655660943001e1dce7585
py-py <valeev0506624980@gmail.com>
2019-01-02 00:31:31 +0200

    fix labalcard

--- components/Labels/LabelCard.vue
+++ components/Labels/LabelCard.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
     <div class="card">
+      <div class="card-header">
+        <h6 class="card-title mb-0">
+          <nuxt-link :to="/labels/+label.id">{{label.name}}</nuxt-link>
+        </h6>
+      </div>
       <nuxt-link :to="/labels/+label.id">
-        <img class="card-img-top" :src="label.image_url" alt>
+        <img class="card-img" :src="label.image_url" alt>
       </nuxt-link>
       <div class="card-body">
-        <h4 class="card-title">
-          <nuxt-link :to="/labels/+label.id">{{label.name}}</nuxt-link>
-        </h4>
         <div class="card-details">
           <div class="row">
             <div class="col-6 pr-1">Производитель:</div>
@@ -28,32 +30,32 @@
 </template>
 
 <script>
-import Moment from 'moment';
+  import Moment from 'moment';
 
-export default {
-  props: {
-    label: {
-      type: Object,
-      required: true
+  export default {
+    props: {
+      label: {
+        type: Object,
+        required: true
+      }
+    },
+    methods: {
+      getMomentFromNow: function (ts) {
+        return Moment(ts).fromNow();
+      },
+    },
+    computed: {
+      kindName() {
+        let kind = this.$store.getters.loadedKinds.find(element => element.id === this.label.kind);
+        return kind.name;
+      },
+      manufactoryName() {
+        let manufactory = this.$store.getters.loadedManufactories.find(element => element.id === this.label.manufactory);
+        return manufactory.name;
+      },
     }
-  },
-  methods: {
-    getMomentFromNow: function(ts) {
-      return Moment(ts).fromNow();
-    },
-  },
-  computed: {
-    kindName() {
-      let kind = this.$store.getters.loadedKinds.find(element=>element.id === this.label.kind);
-      return kind.name;
-    },
-    manufactoryName() {
-      let manufactory = this.$store.getters.loadedManufactories.find(element=>element.id === this.label.manufactory);
-      return manufactory.name;
-    },
-  }
 
-};
+  };
 </script>
 
 <style lang="scss" scoped>
